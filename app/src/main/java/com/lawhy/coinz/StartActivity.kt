799cb3b4a22dac34f175e405629250eafc9dbbda
lawhy <lawhy0729@gmail.com>
2018-09-27 10:06:58 +0800

    init

---
+++ app/src/main/java/com/lawhy/coinz/StartActivity.kt
@@ -0,0 +1,17 @@
+package com.lawhy.coinz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+    }
+
+}
