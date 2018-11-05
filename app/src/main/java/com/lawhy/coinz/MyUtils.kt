73b3a42ea2fe0f86833b99bbcffe6f8c90a9bfa8
lawhy <lawhy0729@gmail.com>
2018-11-05 11:11:53 +0800

    Delete unnecessary code.

--- app/src/main/java/com/lawhy/coinz/MyUtils.kt
+++ app/src/main/java/com/lawhy/coinz/MyUtils.kt
@@ -1,21 +1,31 @@
 package com.lawhy.coinz
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.LayerDrawable
 import android.support.v4.content.ContextCompat
 import android.util.Log
+import com.mapbox.mapboxsdk.annotations.Icon
+import com.mapbox.mapboxsdk.annotations.IconFactory
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-/* Just some utility functions that might be used somewhere in the app */
-
-class MyUtil {
+class MyUtils {
+    /**
+     * Some handy Utils:
+     * 1. getCurrentDate: Current Date in yyyy/MM/dd
+     * 2. symbolDrawable: Get Icon according to symbol
+     * 3. drawableToIcon: Converting any Drawable to an Icon, for use as a marker icon.
+     * 4. combineDrawable: Overlap two drawables and create a new one
+     */
 
     fun getCurrentDate():String {
         val current = LocalDateTime.now()
         val formatter =  DateTimeFormatter.ofPattern("yyyy/MM/dd")
         val currentDate = current.format(formatter)
-        Log.i("CurrentDate", "$currentDate")
+        Log.i("CurrentDate", currentDate)
         return currentDate.trim()
     }
 
@@ -35,6 +45,22 @@ class MyUtil {
             else -> Log.d(context.packageName, "Invalid number on a coin is detected!")
         }
         return icNumber
+    }
+
+    fun drawableToIcon(context: Context, vectorDrawable: Drawable): Icon {
+        val bitmap = Bitmap.createBitmap(vectorDrawable.intrinsicWidth,
+                vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        vectorDrawable.draw(canvas)
+        return IconFactory.getInstance(context).fromBitmap(bitmap)
+    }
+
+    fun combineDrawable(d1: Drawable?, d2: Drawable?): Drawable {
+        val layers = arrayOfNulls<Drawable>(2)
+        layers[0] = d1
+        layers[1] = d2
+        return LayerDrawable(layers)
     }
 
 }
